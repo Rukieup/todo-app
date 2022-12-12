34c99144d2d99b726512a21a4aa6b7d98b4b5dfd
Rukieup <jominhee1221@gmail.com>
2022-12-12 12:25:15 +0900

    feat: Todo 수정 및 삭제 기능 구현

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const { Todo } = require("../models");
 const router = express.Router();
-
 // 기본주소: localhost:PORT/
-
 // GET localhost:PORT/todos - show all todos (READ)
 router.get("/todos", async (req, res) => {
   // Todo.findAll().then((data) => {
-  //   console.log(data);
-  //   res.send(data);
+  //     console.log(data);
+  //     res.send(data)
   // });
-
   try {
     let data = await Todo.findAll();
     console.log(data);
@@ -19,13 +16,12 @@ router.get("/todos", async (req, res) => {
     res.send(err);
   }
 });
-
 // POST localhost:PORT/todo - create a new todo (CREATE)
 router.post("/todo", async (req, res) => {
   try {
     let newTodo = await Todo.create({
       title: req.body.title,
-      // done 디폴트값 있기 때문에 안 써줘도 ok
+      // done: req.body.done,
     });
     console.log(newTodo);
     res.send(newTodo);
@@ -34,5 +30,64 @@ router.post("/todo", async (req, res) => {
   }
 });
 
-// 내보내기
+// PATCH localhost:PORT/todo/:todoId - edit a specific todo (UPDATE)
+// 수정 성공시; true -> res.send(true)
+// 수정 실패시; false -> res.send(false)
+router.patch('/todo/:todoId', async (req, res) => {
+  // console.log(req.body); // { title: 'my todo - 수정', done: true }
+  // console.log(req.params); // { todoId: '1' }
+  try {
+    let [isUpdated] = await Todo.update(
+      {
+        title: req.body.title,
+        done: req.body.done,
+      },
+      {
+        where: {
+          id: req.params.todoId,
+        },
+      }
+    );
+    console.log(isUpdated);
+    // 수정 성공시; [ 1 ] -> 1
+    // 수정 실패시; [ 0 ] -> 0
+
+    // !0
+    if (!isUpdated) {
+      return res.send(false);
+    }
+
+    res.send(true);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// DELETE localhost:RORT/todo/:todoId - remove a specific todo (DELETE)
+router.delete('/todo/:todoId', async (req, res) => {
+  // console.log(req.body); // { title: 'my todo - 수정', done: true }
+  // console.log(req.params); // { todoId: '1' }
+  try {
+    let isDeleted = await Todo.destroy(
+      {
+        where: {
+          id: req.params.todoId,
+        },
+      }
+    );
+    console.log(isDeleted);
+    // 수정 성공시; 1
+    // 수정 실패시; 0
+
+    // !0
+    if (!isDeleted) {
+      return res.send(false);
+    }
+
+    res.send(true);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
 module.exports = router;
