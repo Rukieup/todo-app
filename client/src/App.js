2d56b5d9e482d3f23b8f808daa776849d1b3b1f7
Rukieup <jominhee1221@gmail.com>
2022-12-12 15:16:15 +0900

    feat: Todo 리스트 보이기

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,34 @@
+import {useState} from 'react';
+import Todo from "./components/Todo";
 
 const App = () => {
+  const [todoItem, setTodoItem] = useState([
+    {
+      id: 1,
+      title: 'My Todo1',
+      done: false
+    },
+    {
+      id: 2,
+      title: 'My Todo2',
+      done: false
+    },
+    {
+      id: 3,
+      title: 'My Todo3',
+      done: true
+    },
+  ])
 
   return (
-    <div className="App">My Todo App</div>
+    <div className="App">
+    
+      {todoItem.map((todo, idx)=> {
+        return(
+            <Todo key={idx} todo={todo}/>
+        )
+      })}
+    </div>
   );
 }
 
---
+++ client/src/components/Todo.js
@@ -0,0 +1,22 @@
+// 1. 함수형 컴포넌트
+// 2. input(checkbox)와 label을 렌더링하는 컴포넌트!
+// 3. App (부모 컴포넌트)에서 Todo(자식 컴포넌트) 1개를 렌더링
+
+const Todo = ({ todo }) => {
+  const { id, title, done} = todo;
+
+  return (
+    <div className="Todo">
+      <input
+        type="checkbox"
+        id={`todo${id}`}
+        name={`todo${id}`}
+        value={`todo${id}`}
+        defaultChecked={`todo${done}`}
+      />
+      <label htmlFor={`todo${id}`}>{title}</label>
+    </div>
+  );
+};
+
+export default Todo;
